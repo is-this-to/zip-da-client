2c2fcede4bc64f3c5276abd5028bcb1df8d454ce
YEJIN-GIT <yejinaissi@gmail.com>
2026-09-14 14:03:00 +0900

    20260914 | fix: 연속 새로고침시 수정에 따른 main.js 원복

--- src/main.js
+++ src/main.js
@@ -1,27 +1,10 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
-
 import "./style.css";
 import App from "./App.vue";
 import router from "./route/router.js";
-import { useAuthStore } from "./store/auth/useAuthStore.js";
+import { createPinia } from "pinia";
 
-const bootstrap = async () => {
-  const app = createApp(App);
-  const pinia = createPinia();
-
-  app.use(pinia);
-  app.use(router);
-
-  const authStore = useAuthStore(pinia);
-
-  // 새로고침으로 메모리의 Access Token이 사라져도
-  // Refresh Token 쿠키가 유효하면 로그인 상태를 복구한다.
-  await authStore.reissue();
-
-  await router.isReady();
-
+const app = createApp(App).use(createPinia()).use(router);
+router.isReady().then(() => {
   app.mount("#app");
-};
-
-bootstrap();
+});
