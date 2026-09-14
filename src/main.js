9281040e0d23918161485579ae63e7aded4d121f
soo970220 <soo970220@gmail.com>
2026-09-14 13:02:52 +0900

    20260914 | refactor: 인증 초기화 책임 라우터로 정리

--- src/main.js
+++ src/main.js
@@ -4,24 +4,10 @@ import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 import router from "./route/router.js";
-import { useAuthStore } from "./store/auth/useAuthStore.js";
 
-const bootstrap = async () => {
-  const app = createApp(App);
-  const pinia = createPinia();
+const app = createApp(App);
 
-  app.use(pinia);
-  app.use(router);
+app.use(createPinia());
+app.use(router);
 
-  const authStore = useAuthStore(pinia);
-
-  // 새로고침으로 메모리의 Access Token이 사라져도
-  // Refresh Token 쿠키가 유효하면 로그인 상태를 복구한다.
-  await authStore.reissue();
-
-  await router.isReady();
-
-  app.mount("#app");
-};
-
-bootstrap();
+app.mount("#app");
